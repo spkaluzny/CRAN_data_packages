5a6bef4cf7032ecb3e9dec38da479ff12f3d49d3
spkaluzny <spkaluzny@gmail.com>
2021-01-24 19:43:29 -0800

    New packages added, archived ones noted

--- scripts/check_cran.R
+++ scripts/check_cran.R
@@ -1,12 +1,17 @@
 CRAN_db <- tools::CRAN_package_db()
 CRAN_package <- CRAN_db[, "Package"]
 CRAN_title <- CRAN_db[,  "Title"]
-d <- read.table(here::here("dataPkg.csv2"), sep=";", header=TRUE, quote="")
+cran_pkg_file <- here::here(file.path("data", "dataPkg.csv2"))
+archived_pkg_file <- here::here(file.path("data", "archived_dataPkgs.csv2"))
+d <- read.table(cran_pkg_file, sep=";", header=TRUE, quote="")
 match_on_cran <- match(d[, "Package"], CRAN_package, nomatch=-1)
 indx_not_on_cran <- which(match_on_cran < 0)
-not_on_cran <- d[indx_not_on_cran, ]
-write.table(not_on_cran, "archived_dataPkgs.csv2", sep=";", row.names=FALSE,
-  quote=FALSE)
-dnew <- d[-indx_not_on_cran,]
-write.table(dnew, "dataPkg.csv2", sep=";", row.names=FALSE,
-  quote=FALSE)
+if(length(indx_not_on_cran) > 0) {
+  not_on_cran <- d[indx_not_on_cran, ]
+  write.table(not_on_cran, archived_pkg_file,
+    sep=";", row.names=FALSE, quote=FALSE, append=TRUE,
+    col.names=(!file.exists(archived_pkg_file)))
+  dnew <- d[-indx_not_on_cran,]
+  write.table(dnew, cran_pkg_file, sep=";", row.names=FALSE,
+    quote=FALSE)
+}
